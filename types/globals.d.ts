608651e11644395a7a4643ccc1c04e8d4ea02ab1
Avenor <m15673213520@163.com>
2026-07-04 23:03:06 +0800

    fix: handled the css import problem

---
+++ types/globals.d.ts
@@ -0,0 +1,10 @@
+// types/globals.d.ts
+declare module "*.css" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.module.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
